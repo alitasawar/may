24d60ba44a95d8f6fbb6910c5aaf3ebab826c21d
ali <alitasawar1997@yahoo.com>
2021-06-25 11:03:15 -0700

    simple Finall😍

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
-document.style.background="red"
-console.log("hello");
+// document.body.style.background="red"
+// console.log("hello");
 // my = ()=>alert()
+
+setInterval(() => {
+    // window.location.reload() 
+    sec=Math.ceil(Math.random()*999)
+    document.body.style.background=`#${sec}`
+    
+    chrome.tabs.getSelected(null, function(tab) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+      });
+    // var elt = document.createElement("script");
+    // elt.innerHTML = "window.foo = {bar:function(){window.location.reload()}};"
+    // // elt.innerHTML = ""
+    // document.head.appendChild(elt);
+    
+},2000);
+
+
